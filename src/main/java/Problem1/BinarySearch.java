3f699d847bd563bfc496c63bc6ac8f6839891dae
malasidbaerot <53533195+malasidbaerot@users.noreply.github.com>
2020-11-07 18:49:18 -0800

    Hw

--- src/main/java/Problem1/BinarySearch.java
+++ src/main/java/Problem1/BinarySearch.java
@@ -5,7 +5,28 @@ public class BinarySearch {
     // Do not change signature (return type, function name, parameter types)
     // Add a helper function for recursion calls
     public static int binarySearch(int[] data, int target) {
-        // homework
-        return -1;  // place holder
+        return binaryHelper(data,0,data.length-1,target);
+    }
+
+    public static int binaryHelper(int [] data,int start,int end, int target){
+        if(start>end){
+            return -1;
+        }
+
+        int mid= start+(end-start)/2;
+
+        if(target== data[mid]){
+            return mid;
+        }
+
+        else if(target<data[mid]){
+            return binaryHelper(data,start,mid-1,target);
+        }
+
+        else{
+            return binaryHelper(data,mid+1,end,target);
+        }
     }
 }
+
+
--- src/main/java/Problem2/SortList.java
+++ src/main/java/Problem2/SortList.java
@@ -1,5 +1,7 @@
 package Problem2;
 
+import java.util.List;
+
 public class SortList {
     private static final int SENTRY = Integer.MAX_VALUE;
     // DO NOT ADD ANY NEW MEMBER VARIABLE AND MEMBER FUNCTION
@@ -17,12 +19,51 @@ public class SortList {
     }
 
     public static ListNode findMidAndBreak(ListNode head) {
-        // homework
-        return null;
+        if(head==null || head.next==null ){
+            // if list is too small or doesn't exist
+            return null;
+        }
+        ListNode slow = head;
+
+        ListNode fast = head;
+
+
+        // 1 2 3 4 5
+        while( fast.next!= null) {
+            if (fast.next.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            else{
+                fast=fast.next;
+            }
+        }
+
+        slow.next=null;
+        return slow;
     }
 
     public static ListNode mergeLists(ListNode list1, ListNode list2) {
-        // homework
-        return null;
+        ListNode head= new ListNode();
+        ListNode current= head;
+        while(list1 != null && list2 !=null){
+            if(list1.val < list2.val){
+                current.next=list1;
+                list1= list1.next;
+
+            }
+            else{
+                current.next= list2;
+                list2 = list2.next;
+            }
+            current= current.next;
+        }
+        if(list1 != null){
+            current.next= list1;
+        }
+        else if(list2 != null){
+            current.next= list2;
+        }
+        return head.next;
     }
 }
